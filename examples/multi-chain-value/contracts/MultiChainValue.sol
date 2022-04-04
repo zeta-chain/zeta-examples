93e964bc00e1fe22e2d7fd4e5292e6febf092919
Lucas Janon <ljanon@gmail.com>
2022-04-04 19:20:04 -0400

    WIP: multi-chain value

--- examples/multi-chain-value/contracts/MultiChainValue.sol
+++ examples/multi-chain-value/contracts/MultiChainValue.sol
@@ -1,30 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
-import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@zetachain/contracts/ZetaInterfaces.sol";
-import "@zetachain/contracts/ZetaReceiver.sol";
 
-contract MultiChainValue is ZetaReceiver {
+contract MultiChainValue is Ownable {
     address public zetaMpi;
-    address public zetaToken;
+    ZetaMPI internal _zeta;
 
-    ZetaMPI internal _zetaMpi;
-    IERC20 internal _zetaToken;
+    mapping(uint16 => bool) public availableChainIds;
 
-    constructor(address _zetaMpiInput, address _zetaTokenInput) {
+    constructor(address _zetaMpiInput) {
         zetaMpi = _zetaMpiInput;
-        zetaToken = _zetaTokenInput;
-
-        _zetaMpi = ZetaMPI(_zetaMpiInput);
-        _zetaToken = IERC20(_zetaTokenInput);
+        _zeta = ZetaMPI(_zetaMpiInput);
     }
 
-    function onZetaMessage(ZetaInterfaces.ZetaMessage calldata _zetaMessage) external {
-        //
+    function addAvailableChainId(uint16 destinationChainId) external onlyOwner {
+        require(!availableChainIds[destinationChainId], "MultiChainValue: destinationChainId already enabled");
+
+        availableChainIds[destinationChainId] = true;
     }
 
-    function onZetaRevert(ZetaInterfaces.ZetaRevert calldata _zetaRevert) external {
-        //
+    function removeAvailableChainId(uint16 destinationChainId) external onlyOwner {
+        require(availableChainIds[destinationChainId], "MultiChainValue: destinationChainId not available");
+
+        delete availableChainIds[destinationChainId];
+    }
+
+    function send(
+        uint16 destinationChainId,
+        bytes calldata destinationAddress,
+        uint256 zetaAmount
+    ) external {
+        require(availableChainIds[destinationChainId], "MultiChainValue: destinationChainId not available");
+        require(zetaAmount != 0, "MultiChainValue: zetaAmount should be greater than 0");
+
+        _zeta.send(
+            ZetaInterfaces.SendInput({
+                destinationChainId: destinationChainId,
+                destinationAddress: destinationAddress,
+                gasLimit: 2500000,
+                message: abi.encode(),
+                zetaAmount: zetaAmount,
+                zetaParams: abi.encode("")
+            })
+        );
     }
 }
--- examples/multi-chain-value/test/MultiChainValue.spec.ts
+++ examples/multi-chain-value/test/MultiChainValue.spec.ts
@@ -19,8 +19,6 @@ describe("MultiChainValue tests", () => {
   let deployerAddress: string;
   let account1Address: string;
 
-  const encoder = new ethers.utils.AbiCoder();
-
   beforeEach(async () => {
     zetaMPIMockContract = await deployZetaMPIMock();
     multiChainValueContractA = await deployMultiChainValueMock({
@@ -30,10 +28,8 @@ describe("MultiChainValue tests", () => {
       zetaMPIMockAddress: zetaMPIMockContract.address,
     });
 
-    await multiChainValueContractA.setCrossChainID(chainBId);
-    await multiChainValueContractB.setCrossChainID(chainAId);
-    await multiChainValueContractA.setCrossChainAddress(encoder.encode(["address"], [multiChainValueContractB.address]));
-    await multiChainValueContractB.setCrossChainAddress(encoder.encode(["address"], [multiChainValueContractA.address]));
+    await multiChainValueContractA.addAvailableChainId(chainBId);
+    await multiChainValueContractB.addAvailableChainId(chainAId);
 
     accounts = await ethers.getSigners();
     [deployer, account1] = accounts;
@@ -41,9 +37,79 @@ describe("MultiChainValue tests", () => {
     account1Address = account1.address;
   });
 
-  describe("someFunction", () => {
-    it("Should work", async () => {
-      expect(true).to.equal(true);
+  describe("addAvailableChainId", () => {
+    it("Should prevent enabling a chainId that's already enabled", async () => {
+      await (await multiChainValueContractA.addAvailableChainId(1)).wait();
+
+      await expect(multiChainValueContractA.addAvailableChainId(1)).to.be.revertedWith(
+        "MultiChainValue: destinationChainId already enabled"
+      );
+    });
+
+    it("Should enable the provided chainId", async () => {
+      await (await multiChainValueContractA.addAvailableChainId(1)).wait();
+
+      expect(await multiChainValueContractA.availableChainIds(1)).to.equal(true);
+    });
+  });
+
+  describe("removeAvailableChainId", () => {
+    it("Should prevent disabling a chainId that's already disabled", async () => {
+      await expect(multiChainValueContractA.removeAvailableChainId(1)).to.be.revertedWith(
+        "MultiChainValue: destinationChainId not available"
+      );
+    });
+
+    it("Should disable the provided chainId", async () => {
+      await (await multiChainValueContractA.addAvailableChainId(1)).wait();
+      expect(await multiChainValueContractA.availableChainIds(1)).to.equal(true);
+
+      await (await multiChainValueContractA.removeAvailableChainId(1)).wait();
+      expect(await multiChainValueContractA.availableChainIds(1)).to.equal(false);
+    });
+  });
+
+  describe("send", () => {
+    it("Should prevent sending value to a disabled chainId", async () => {
+      await expect(multiChainValueContractA.send(1, account1Address, 100_000)).to.be.revertedWith(
+        "MultiChainValue: destinationChainId not available"
+      );
+    });
+
+    it("Should prevent sending 0 value", async () => {
+      await (await multiChainValueContractA.addAvailableChainId(1)).wait();
+
+      await expect(multiChainValueContractA.send(1, account1Address, 0)).to.be.revertedWith(
+        "MultiChainValue: zetaAmount should be greater than 0"
+      );
+    });
+
+    it("Should prevent sending if the account has no Zeta balance", async () => {
+      await (await multiChainValueContractA.addAvailableChainId(1)).wait();
+
+      /**
+       * @todo (lucas): implement after merging zeta-examples and zeta-contracts
+       */
+    });
+
+    it("Should prevent sending value to an invalid address", async () => {
+      await (await multiChainValueContractA.addAvailableChainId(1)).wait();
+
+      /**
+       * @todo (lucas): create utility to validate addresses
+       */
+    });
+
+    describe("Given a valid input", () => {
+      it("Should send value", async () => {
+        await (await multiChainValueContractA.addAvailableChainId(1)).wait();
+
+        await multiChainValueContractA.send(1, account1Address, 100_000);
+
+        /**
+         * @todo (lucas): improve ZetaMPIMock to emulate actual functionality
+         */
+      });
     });
   });
 });
