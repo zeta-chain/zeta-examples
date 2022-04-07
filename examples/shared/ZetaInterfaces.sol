0e2a19ea0f788a7074814e91058787a27d0a659a
Lucas Janon <ljanon@gmail.com>
2022-04-07 01:56:01 -0400

    Add comments to interfaces

--- examples/shared/ZetaInterfaces.sol
+++ examples/shared/ZetaInterfaces.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.12;
 
 interface ZetaInterfaces {
+    /**
+     * @dev Use SendInput to interact with our Message Passing Interface: zeta.send(SendInput)
+     */
     struct SendInput {
         uint256 destinationChainId;
         bytes destinationAddress;
@@ -11,6 +14,10 @@ interface ZetaInterfaces {
         bytes zetaParams;
     }
 
+    /**
+     * @dev Our Message Passing Interface will call your contract's onReceive using this interface
+     * onReceive will be called when a cross-chain message is delivered to your contract
+     */
     struct ZetaMessage {
         bytes originSenderAddress;
         uint256 originChainId;
@@ -19,6 +26,10 @@ interface ZetaInterfaces {
         bytes message;
     }
 
+    /**
+     * @dev Our Message Passing Interface will call your contract's onRevert using this interface
+     * onRevert will be called when a cross-chain message reverted and it's useful to recover to the original state
+     */
     struct ZetaRevert {
         address originSenderAddress;
         uint256 originChainId;
@@ -30,5 +41,8 @@ interface ZetaInterfaces {
 }
 
 interface ZetaMPI {
-    function send(ZetaInterfaces.SendInput calldata _input) external;
+    /**
+     * @dev Sending value and data cross-chain is as easy as calling zeta.send(SendInput)
+     */
+    function send(ZetaInterfaces.SendInput calldata input) external;
 }
